be15b85465c51bf4bfc1c11a85858b7d4650f48c
Aleksei <9195174@gmail.com>
2024-12-17 03:45:09 +0300

    commit

--- TicketApi/Models/TicketsystemContext.cs
+++ TicketApi/Models/TicketsystemContext.cs
@@ -33,6 +33,7 @@ public partial class TicketsystemContext : DbContext
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=ticketsystem;Username=postgres;Password=11111111");
 
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Job>(entity =>
